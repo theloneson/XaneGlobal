48deec8958bf43f944518f387d50261e4e824e12
theloneson <l.o.n.e.s.o.n456@gmail.com>
2025-08-25 04:40:25 -0500

    update

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -47,7 +47,7 @@ const FeaturesSection = () => {
           <div className="order-1 lg:order-2">
             <div className="mb-12">
               <span className="text-6xl font-bold text-text-secondary/20"></span>
-              <h2 className="text-4xl md:text-5xl font-bold text-foreground mb-6">
+              <h2 className="text-4xl md:text-7xl font-bold text-foreground mb-6">
                 More Than Just
                 <br />
                 <span className="text-brand-blue">Trading</span>
